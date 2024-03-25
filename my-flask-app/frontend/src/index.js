554b20d37699203eb833f526be5b17129d053aa9
unknown <2725594056@qq.com>
2024-03-25 21:00:49 +0800

    325

---
+++ my-flask-app/frontend/src/index.js
@@ -0,0 +1,15 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+import Test from './test'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    {/* <Test /> */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
