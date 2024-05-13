40eaf77132ef5e45f34b2dfbeadb8084da9fba04
Genghong <godhu777777@gmail.com>
2024-05-13 19:23:58 +0800

    server.py: removed redudant code

--- server.py
+++ server.py
@@ -61,45 +61,8 @@ def home():
         # -----------------从pod抓信息(GET 请求）-----------------
         
         
-        result = your_function(input_str)  # 调用你的函数，并传入用户输入的字符串
-        return render_template('index.html', result=result)
+        return render_template('index.html', result=response.text)
     return render_template('index.html')
-
-def your_function(text):
-
-    # 加载已保存的模型和tokenizer
-    model = BertForTokenClassification.from_pretrained("/home/ghhu/study/docker/docker-ner/test-ner")
-    tokenizer = BertTokenizer.from_pretrained("/home/ghhu/study/docker/docker-ner/test-ner")
-
-    # 输入文本
-    # TODO: can combine with frontend(FLASK) to make interactive input
-    # Done
-
-    # 对输入文本进行标记化
-    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
-
-    # 进行预测
-    with torch.no_grad():
-        outputs = model(**inputs)
-
-    # 处理预测结果
-    predicted_labels = torch.argmax(outputs.logits, axis=-1)
-
-    # 获取实体类型标签映射
-    label_map = {i: label for i, label in enumerate(model.config.id2label)}
-
-    # label_id to label
-    labels = ['O', 'B-PER', 'I-PER', 'B-ORG', 'I-ORG', 'B-LOC', 'I-LOC', 'B-MISC', 'I-MISC']
-
-    res = ""
-
-    # 显示预测结果和实体类型
-    # TODO: can combine with frontend to make interative output
-    for token, label_id in zip(tokenizer.convert_ids_to_tokens(inputs["input_ids"][0]), predicted_labels[0]):
-        print(f"{token}: {labels[label_map[label_id.item()]]}")
-        res += f"{token}: {labels[label_map[label_id.item()]]}" + "  " 
-    
-    return res
 
     
 # receive message from pod
