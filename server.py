89ba5b8281ba02c5659ec45a44951ebc48ac95ba
Genghong Hu <godhu777777@gmail.com>
2024-05-15 01:21:17 +0800

    Updated: communication done

--- server.py
+++ server.py
@@ -64,4 +64,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug = True, port=5000)
+    app.run(host="0.0.0.0", debug = True, port=80)
